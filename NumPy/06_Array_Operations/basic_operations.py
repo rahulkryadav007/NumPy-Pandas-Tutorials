d711e99a6d64cfe016f63724a64a8ff727ed5115
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:30:49 +0530

    Add NumPy/06_Array_Operations/basic_operations.py

---
+++ NumPy/06_Array_Operations/basic_operations.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([10, 20, 30])
+
+print("Addition:", arr + 5)
+print("Subtraction:", arr - 5)
+print("Multiplication:", arr * 2)
+print("Division:", arr / 2)
+print("Square:", arr ** 2)
