bf1ef6055703edce0e71a1988e53d21c7416a6c4
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:35:09 +0530

    Add Hinglish comments to NumPy/04_Indexing/indexing_1d.py

--- NumPy/04_Indexing/indexing_1d.py
+++ NumPy/04_Indexing/indexing_1d.py
@@ -1,7 +1,8 @@
-import numpy as np
+import numpy as np  # NumPy library import kar rahe hain
 
+# 1D array create kar rahe hain
 arr = np.array([10, 20, 30, 40, 50])
 
-print("First:", arr[0])
-print("Third:", arr[2])
-print("Last:", arr[-1])
+print("First:", arr[0])  # Index 0 se first value access
+print("Third:", arr[2])  # Index 2 se third value access
+print("Last:", arr[-1])  # Negative indexing se last value access
