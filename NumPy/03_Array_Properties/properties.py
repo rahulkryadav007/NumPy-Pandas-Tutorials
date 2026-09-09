3642a3baadcae95889971faf096834896d3476d5
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:30:21 +0530

    Add NumPy/03_Array_Properties/properties.py

---
+++ NumPy/03_Array_Properties/properties.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+marks = np.array([
+    [80, 75, 90],
+    [85, 95, 70]
+])
+
+print("Array:")
+print(marks)
+
+print("Shape:", marks.shape)
+print("Size:", marks.size)
+print("Dimensions:", marks.ndim)
+print("Data Type:", marks.dtype)
