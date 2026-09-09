e0e28a64108737911e1c29f7b87655ad271116f0
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:33:32 +0530

    Add Hinglish comments to NumPy/02_Array_Creation/zeros.py

--- NumPy/02_Array_Creation/zeros.py
+++ NumPy/02_Array_Creation/zeros.py
@@ -1,5 +1,6 @@
-import numpy as np
+import numpy as np  # NumPy library import kar rahe hain
 
+# 3 rows aur 3 columns ka array bana rahe hain jisme sab values 0 hain
 arr = np.zeros((3, 3))
 
-print(arr)
+print(arr)  # Zero matrix print kar rahe hain
