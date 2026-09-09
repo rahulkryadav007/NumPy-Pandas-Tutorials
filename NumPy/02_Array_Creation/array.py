412b6af47b1c5455123b6ea55580890b5c3bb8b8
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:33:29 +0530

    Add Hinglish comments to NumPy/02_Array_Creation/array.py

--- NumPy/02_Array_Creation/array.py
+++ NumPy/02_Array_Creation/array.py
@@ -1,5 +1,6 @@
-import numpy as np
+import numpy as np  # NumPy library import kar rahe hain
 
+# Python values se ek simple NumPy array create kar rahe hain
 arr = np.array([10, 20, 30, 40, 50])
 
-print(arr)
+print(arr)  # Created array ko print kar rahe hain
