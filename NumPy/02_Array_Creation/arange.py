c4fd4e42315c1d7e7c7a5439d7fa0033d02a8c18
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:33:42 +0530

    Add Hinglish comments to NumPy/02_Array_Creation/arange.py

--- NumPy/02_Array_Creation/arange.py
+++ NumPy/02_Array_Creation/arange.py
@@ -1,5 +1,6 @@
-import numpy as np
+import numpy as np  # NumPy library import kar rahe hain
 
+# 0 se start karke 20 se pehle tak har baar 2 ka gap rakhkar values create kar rahe hain
 numbers = np.arange(0, 20, 2)
 
-print(numbers)
+print(numbers)  # Generated sequence print kar rahe hain
