90baadfc829b9f79106a23fe83a4c5d26079035e
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:33:35 +0530

    Add Hinglish comments to NumPy/02_Array_Creation/ones.py

--- NumPy/02_Array_Creation/ones.py
+++ NumPy/02_Array_Creation/ones.py
@@ -1,5 +1,6 @@
-import numpy as np
+import numpy as np  # NumPy library import kar rahe hain
 
+# 2 rows aur 4 columns ka array bana rahe hain jisme sab values 1 hain
 arr = np.ones((2, 4))
 
-print(arr)
+print(arr)  # Ones array print kar rahe hain
