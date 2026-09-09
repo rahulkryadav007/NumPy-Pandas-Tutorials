1364897b80b9cedd06bf04689737b6099c9c7b55
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-09-10 01:30:44 +0530

    Add NumPy/06_Array_Operations/grace_marks.py

---
+++ NumPy/06_Array_Operations/grace_marks.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+marks = np.array([70, 80, 90])
+bonus_marks = 5
+
+updated_marks = marks + bonus_marks
+
+print("Original Marks:", marks)
+print("Updated Marks:", updated_marks)
